ed11b3a482527389fb0441ed84c75d7b2c46e3a9
Jackson H <jackson.hardaway@gmail.com>
2021-05-20 15:38:02 -0700

    remove unused import

--- src/main/java/me/jaackson/fireflies/FirefliesPlugin.java
+++ src/main/java/me/jaackson/fireflies/FirefliesPlugin.java
@@ -8,7 +8,6 @@ import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -45,7 +44,7 @@ public class FirefliesPlugin extends JavaPlugin implements Listener {
 
         this.getServer().getPluginManager().registerEvents(this, this);
     }
-    
+
     @EventHandler
     public void onEvent(ServerTickEndEvent event) {
         Bukkit.getOnlinePlayers().forEach(player -> {
